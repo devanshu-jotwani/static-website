bc689119c4e9ac1d755cdb107d40c3df2a9be8ed
devanshu-jotwani <devanshujotwani14@gmail.com>
2021-12-26 22:58:05 +0530

    first commit

---
+++ personal-portfolio/mypf/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+from .models import Contact
+# Create your views here.
+
+
+# Home/Index View
+def index(request):
+    # check post method and request type
+    
+        return render(request, 'mypf/home.html')
+
+
+#Portfolio View
+def portfolio(request):
+    return render(request, 'mypf/portfolio.html')
+
+
+# Contact View
+def contact(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        form = Contact(email=email, subject=subject, message=message)
+        # Saving Form data to Database
+        form.save()
+
+        return render(request, 'mypf/contact.html')
+    else:
+        return render(request, 'mypf/contact.html')
